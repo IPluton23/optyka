10b2aacaebb443484104bf0212ccb71e50e794f9
Karol <plutopradzynskikarol@gmail.com>
2025-12-06 19:00:54 +0100

    komentarze

--- wykres_soczewki.py
+++ wykres_soczewki.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x_c1, z_c = 0,0   # koordynaty centrum sferY 1 (lewej)
-x_c2 = 0    # koordynaty centrum sfery 2 (prawej), są na tej samej wysokości z_c
+x_c1, z_c = 0,0     # koordynaty centrum sferY 1 (lewej)
+x_c2 = 0            # koordynaty centrum sfery 2 (prawej), są na tej samej wysokości z_c
 R = 5
 wysokosc = 10       # wysokosc soczewki
 
@@ -10,8 +10,8 @@ size = 16           # szerokość wykresu
 
 
 z = np.linspace(-wysokosc/2, wysokosc/2, 500)
-x_upper = x_c1 + np.sqrt(R**2 - z**2)-(R-wysokosc/2)    # Prawa część soczewki - pochodzi od sfery lewej
-x_lower = x_c2 - (np.sqrt(R**2 - z**2)+(R-wysokosc/2))  # Lewa część soczewki
+x_upper = x_c1 + (np.sqrt(R**2 - z**2)-(R-wysokosc/2))      # Prawa część soczewki - pochodzi od sfery lewej
+x_lower = x_c2 - (np.sqrt(R**2 - z**2)+(R-wysokosc/2))      # Lewa część soczewki
 
 
 plt.figure(figsize=(6,6))
