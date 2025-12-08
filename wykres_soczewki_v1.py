3c8e56712a9dcf50125710e65b42a92bb07f0881
IPluton23 <137773017+IPluton23@users.noreply.github.com>
2025-12-08 17:12:14 +0100

    Update wykres_soczewki_v1.py

--- wykres_soczewki_v1.py
+++ wykres_soczewki_v1.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+size = 3
 """
+Dodanie aparture tam gdzie brakuje
+
 musimy dodac jeszcze zakresy parametrów, tak żeby soczewki miały sens
 
 dodatkowo wartości pod pierwiastkiem dla nie których parametrów są ujemne.
@@ -10,27 +13,25 @@ a tej prostej nie chcemy
 wypełnienie soczewek jeszcze nie działa. myślę czy nie użyć fill_between() zamiast fill_betweenx()
 """
 
+
 ## soczewki dwuwypukłe
 def dwuwypukle(x_1, x_2, R1, R2):
-    size=3
 
-    prosta = (x_1**2-x_2**2-R1**2+R2**2)/(2*(x_1-x_2))  # prosta w punktach przecięcia dwóch okręgów
+    prosta = (x_1 ** 2 - x_2 ** 2 - R1 ** 2 + R2 ** 2) / (2 * (x_1 - x_2))  # prosta w punktach przecięcia dwóch okręgów
     # print('prosta', prosta)
 
-    # równania soczewki nr 1 (prawej)
+# równania soczewki nr 1 (okrag prawy)
+    x1 = np.linspace(x_1 - R1, prosta, 500)
+    z1_upper = np.sqrt(R1 ** 2 -(x1-x_1)**2 )   # górna półkula
+    z1_lower = -z1_upper                        # dolna półkula
 
-    x1=np.linspace(x_1-R1, prosta, 500)
-    z1_upper=np.sqrt(R1**2+2*x1*x_1-x1**2-x_1**2)
-    z1_lower=-np.sqrt(R1**2+2*x1*x_1-x1**2-x_1**2)
+# równania soczewki nr 2 (okrag lewy)
+    x2 = np.linspace(prosta, x_2 + R2, 500)
+    z2_upper = np.sqrt(R2 ** 2 - (x2 - x_2)**2) # górna półkula
+    z2_lower = -z2_upper                        # dolna półkula
 
-    # równania soczewki nr 2 (lewej)
-
-    x2=np.linspace(prosta, x_2+R2, 500)
-    z2_upper=np.sqrt(R2**2+2*x2*x_2-x2**2-x_2**2)
-    z2_lower=-np.sqrt(R2**2+2*x2*x_2-x2**2-x_2**2)
-
-    plt.figure(figsize=(6,6))
-    # plt.fill_betweenx(z+z_c, x_lower, x_upper, color='skyblue')
+    plt.figure(figsize=(6, 6))
+    #plt.fill_betweenx(z+z_c, x_lower, x_upper, color='skyblue')
     plt.plot(x1, z1_upper, 'b')
     plt.plot(x1, z1_lower, 'b')
     plt.plot(x2, z2_upper, 'b')
@@ -38,88 +39,82 @@ def dwuwypukle(x_1, x_2, R1, R2):
 
     plt.xlim(-size, size)
     plt.ylim(-size, size)
-    plt.gca().set_aspect('equal')
-    plt.grid(True)
+    #plt.gca().set_aspect('equal')
+    plt.grid(True)                  # NA POTEM czy zostawić grid czy wyrzucić
     plt.xlabel('x')
     plt.ylabel('z')
     plt.show()
+#dwuwypukle(1,-1.2,1.16,2)
 
-
-# dwuwypukle(1,-1.2,1.16,2)
 
 ## soczewki dwuwklęsłe
+def dwuwklesle(x_1, x_2, R1, R2, a): # a - aperture
+    ograniczenie_1 = (
+                x_1 - np.sqrt(R1 ** 2 - a ** 2))                            # proste pionowa, w punktach przecięcia y=a i y=-a (aperture) z okręgami
+    ograniczenie_2 = (
+                x_2 + np.sqrt(R2 ** 2 - a ** 2))                            # proste pionowa, w punktach przecięcia y=a i y=-a (aperture) z okręgami
+    prosta = (x_1 ** 2 - x_2 ** 2 - R1 ** 2 + R2 ** 2) / (2 * (x_1 - x_2))  # prosta przechodząca przez środek soczewki
 
-def dwuwklesle(x_1, x_2, R1, R2, a):
-  
-    size=3
-    y=a
-    ograniczenie_1=(x_1-np.sqrt(R1**2-a**2))    # proste x, w punktach przecięcia y=a i y=-a (aperture) z okręgami
-    ograniczenie_2=(x_2+np.sqrt(R2**2-a**2))    # proste x, w punktach przecięcia y=a i y=-a (aperture) z okręgami
-    prosta = (x_1**2-x_2**2-R1**2+R2**2)/(2*(x_1-x_2))      # prosta przechodząca przez środek soczewki
-    x1=np.linspace(ograniczenie_2, ograniczenie_1, 500)
-    z1_upper=np.sqrt(R1**2+2*x1*x_1-x1**2-x_1**2)
-    z1_lower=-np.sqrt(R1**2+2*x1*x_1-x1**2-x_1**2)
+# okrąg prawy
+    x1 = np.linspace(ograniczenie_2, ograniczenie_1, 500)
+    z1_upper = np.sqrt(R1 ** 2 - (x1 - x_1)**2)
+    z1_lower = -z1_upper
+#okrąg lewy
+    x2 = np.linspace(ograniczenie_2, ograniczenie_1, 500)
+    z2_upper = np.sqrt(np.clip(R2 ** 2 - (x2 - x_2)**2))
+    z2_lower = -z2_upper
 
-    x2=np.linspace(ograniczenie_2, ograniczenie_1, 500)
-    z2_upper=np.sqrt(R2**2+2*x2*x_2-x2**2-x_2**2)
-    z2_lower=-np.sqrt(R2**2+2*x2*x_2-x2**2-x_2**2)
-
-    plt.figure(figsize=(6,6))
+    plt.figure(figsize=(6, 6))
     # plt.fill_betweenx(z1_upper, x1, x1, where=z1_upper>=z1_lower, color='lightblue', alpha=0.5) ## jeszcze nie działa
     plt.plot(x1, z1_upper, 'b')
     plt.plot(x1, z1_lower, 'b')
     plt.plot(x2, z2_upper, 'b')
     plt.plot(x2, z2_lower, 'b')
-    plt.plot(x1, np.full_like(x1, y), 'b')
-    plt.plot(x2, np.full_like(x2, -y), 'b')
+    plt.plot(x1, np.full_like(x1, a), 'b')
+    plt.plot(x2, np.full_like(x2, -a), 'b')
     plt.xlim(-size, size)
     plt.ylim(-size, size)
-    plt.gca().set_aspect('equal')
+    #plt.gca().set_aspect('equal')
     plt.grid(True)
     plt.xlabel('x')
     plt.ylabel('z')
     plt.show()
+#dwuwklesle(0.7,-1.2,0.8,-0.7,0.6)
 
-# dwuwklesle(0.7,-1.2,0.8,-0.7,0.6)
-
-def plasko_wypukle(x_1, R1, a):
+def plasko_wypukle(x_1, R1, ograniczenie):
     """
     Docstring for plasko_wypukle
-    
+
     :param x_1: Description
     :param R1: Description
-    :param a: ograniczenie soczewki     a<x_1
+    :param ograniczenie: ograniczenie soczewki   prosta < x_1
     """
+    x1 = np.linspace(x_1 - R1, ograniczenie, 500)
+    z1_upper = np.sqrt(np.clip(R1 ** 2 - (x1 - x_1)**2, 0, None))
+    z1_lower = -z1_upper
 
-    size=3
- 
-    prosta=a
-    x1=np.linspace(x_1-R1, prosta, 500)
-    z1_upper=np.sqrt(np.clip(R1**2+2*x1*x_1-x1**2-x_1**2, 0, None))
-    z1_lower=-z1_upper
-
-    plt.figure(figsize=(6,6))
+    plt.figure(figsize=(6, 6))
     # plt.fill_betweenx(z+z_c, x_lower, x_upper, color='skyblue')
-    # rysowanie pionowej prostej x=a w zakresie soczewki:
-    z_max = np.max(z1_upper)    #zakresy prostej ograniczającej soczewkę - płaska cześć soczewki
+# rysowanie pionowej prostej x=prosta w zakresie soczewki:
+    z_max = np.max(z1_upper)  # zakresy prostej ograniczającej soczewkę - płaska cześć soczewki
     z_min = np.min(z1_lower)
-    plt.plot([a, a], [z_min, z_max], 'b')
+    plt.plot([ograniczenie, ograniczenie], [z_min, z_max], 'b')
+
     plt.plot(x1, z1_upper, 'b')
     plt.plot(x1, z1_lower, 'b')
     plt.xlim(-size, size)
     plt.ylim(-size, size)
-    plt.gca().set_aspect('equal')
+    #plt.gca().set_aspect('equal')
     plt.grid(True)
     plt.xlabel('x')
     plt.ylabel('z')
     plt.show()
-
-# plasko_wypukle(0.64,1,0.2)
+#plasko_wypukle(0.64,1,0.2)
 
 def plasko_wklesle(x_2, R2, a):
     """
     Docstring for plasko_wklesle
-    
+
     :param x_2: Description
     :param R2: Description
     :param a:       a <= |x_2|
@@ -127,26 +122,25 @@ def plasko_wklesle(x_2, R2, a):
     x = x_2 + R_2 + a/2
 
     """
-    size=3
-    y=a
+    ograniczenie = (
+                x_2 + np.sqrt(R2 ** 2 - a ** 2))  # proste pionowe, w punktach przecięcia y=a i y=-a (aperture) z okręgami
 
-    ograniczenie_2=(x_2+np.sqrt(R2**2-a**2))    # proste x, w punktach przecięcia y=a i y=-a (aperture) z okręgami
-    
-    x2=np.linspace(ograniczenie_2, x_2+R2+(a/2), 500)
+    x2 = np.linspace(ograniczenie, x_2 + R2 + (a / 2), 500)
     # z2_upper=np.sqrt(R2**2+2*x2*x_2-x2**2-x_2**2)
-    z2_upper = np.sqrt(np.clip(R2**2 + 2*x2*x_2 - x2**2 - x_2**2, 0, None))
-    z2_lower=-z2_upper
-    
-    plt.figure(figsize=(6,6))
+    z2_upper = np.sqrt(np.clip(R2 ** 2 + 2 * x2 * x_2 - x2 ** 2 - x_2 ** 2, 0, None))
+    z2_lower = -z2_upper
+
+    plt.figure(figsize=(6, 6))
     # plt.fill_betweenx(z+z_c, x_lower, x_upper, color='skyblue')
-    # rysowanie pionowej prostej x=a w zakresie soczewki:
-    z_max = np.max(z2_upper)    #zakresy prostej ograniczającej soczewkę - płaska cześć soczewki
+# rysowanie pionowej prostej x=ograniczenie w zakresie soczewki:
+    z_max = np.max(z2_upper)  # zakresy prostej ograniczającej soczewkę - płaska cześć soczewki
     z_min = np.min(z2_lower)
-    plt.plot([x_2+R2+(a/2), x_2+R2+(a/2)], [z_min, z_max], 'b')
+    plt.plot([x_2 + R2 + (a / 2), x_2 + R2 + (a / 2)], [z_min, z_max], 'b')
+
     plt.plot(x2, z2_upper, 'b')
     plt.plot(x2, z2_lower, 'b')
-    plt.plot(x2, np.full_like(x2, y), 'b')
-    plt.plot(x2, np.full_like(x2, -y), 'b')
+    plt.plot(x2, np.full_like(x2, a), 'b')
+    plt.plot(x2, np.full_like(x2, -a), 'b')
     plt.xlim(-size, size)
     plt.ylim(-size, size)
     plt.gca().set_aspect('equal')
@@ -154,5 +148,4 @@ def plasko_wklesle(x_2, R2, a):
     plt.xlabel('x')
     plt.ylabel('z')
     plt.show()
-
-# plasko_wklesle(-1.4,1.1,0.85)
+#plasko_wklesle(-1.4,1.1,0.85)
